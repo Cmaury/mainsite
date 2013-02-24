b60ce136943f7eee73bffc60cb7bab168ecb66b0
cmaury <chrmaury@gmail.com>
2013-02-24 10:53:57 -0800

    adding form js

---
+++ Template/js/js.js
@@ -0,0 +1,14 @@
+$("form").submit(function(e) {
+    e.preventDefault(); 
+    var $this = $(this);
+    $.post(
+        $this.attr("action"),
+        $this.serialize(),
+        function(data) {
+        	console.log('did this work?')
+        	form_alert.style.display = 'block'
+        	form_alert_message.innerHTML = form_alert.getAttribute("message")
+        },
+        "json" 
+    );
+});
